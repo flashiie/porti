0e4d7db3d8d833a766b5e4ec409d50e9573bf30f
itsflashii <furiadev2020@gmail.com>
2022-08-22 10:34:03 -0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+const App = () => {
+  return (
+    <>
+    <div id="row--left"></div>
+    <div id="row--right"></div>
+    <div id="container">
+      <div id="user">
+        <img id="avatar" src="https://avatars.githubusercontent.com/u/85264247?v=4" alt="" />
+        <h1>Vítor Ribeiro</h1>
+        <p>@flashii</p>
+        <h3>CTO @varselcombr Success is not accidental, it's hard work, perseverance, learning, sacrifice and love for what we do.</h3>
+        <div id="ido">
+            <p>📚 Studying</p>
+            <p>🎥 Videomaker</p>
+            <p>👨🏼‍💻 Programmer</p>
+            <p>🌍 Philanthropist</p>
+            <p>🏝️ Designer</p>
+        </div>
+        <a href="https://instagram.com/rocketrp" target="_blank"> 
+          <img src={'https://img.shields.io/badge/-Instagram-%23E4405F?style=flat-square&logo=instagram&logoColor=white'}target="_blank" />
+        </a>
+        <a href="mailto://buss.vitorsheik@gmail.com" target="_blank"> 
+          <img src={'https://img.shields.io/badge/Gmail-D14836?style=flat-square&logo=gmail&logoColor=white'}target="_blank" />
+        </a>
+        <a href="https://github.com/flashiie" target="_blank"> 
+          <img src={'https://img.shields.io/badge/Github-333232?style=flat-square&logo=github&logoColor=dark'}target="_blank" />
+        </a>
+        <div id="track">
+        <iframe width="100%" height="315" src="https://www.youtube.com/embed/B12JqWlZaXw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>        </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App;
